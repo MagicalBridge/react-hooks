85097e1d01543e8509f669be8fa4b56dd708f6d6
褚鹏飞 <450793585@qq.com>
2019-11-09 10:42:29 +0800

    初始化项目

--- learn-hooks/src/components/simpleHooks.jsx
+++ learn-hooks/src/components/simpleHooks.jsx
@@ -1,5 +1,3 @@
-### 1、我自己在component 中创建了一个组件 在App.jsx中引用
-```js
 import React, { Component } from 'react'
 
 class simpleHooks extends Component {
@@ -22,6 +20,3 @@ class simpleHooks extends Component {
 }
 
 export default simpleHooks
-```
-准备对这个简单的例子进行改造成react-hooks版本。
-
