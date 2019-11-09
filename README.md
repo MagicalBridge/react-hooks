958e9e1121c4940492e7a85975ed1f70e22384b9
褚鹏飞 <450793585@qq.com>
2019-11-09 11:28:17 +0800

    回顾之前的组件复用的技巧

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ```js
 import React, { Component } from 'react'
 
-class simpleHooks extends Component {
+class SimpleHooks extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -21,7 +21,32 @@ class simpleHooks extends Component {
   }
 }
 
-export default simpleHooks
+export default SimpleHooks
 ```
 准备对这个简单的例子进行改造成react-hooks版本。
+
+```js
+import React, { useState } from 'react'
+function SimpleHooks() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        click me
+      </button>
+    </div>
+  )
+}
+export default SimpleHooks
+```
+
+### React 为什么要搞一个Hooks？
+想要复用一个组件是在是太麻烦了！
+
+react的思想是将页面拆分成一个个独立的，可以复用的组件，并且用自上而下的单向数据流将这些组件串联起来，但是实际的使用过程中
+我们的项目上很多大型组件冗长且组件难以复用，尤其哪些class组件，本身包含了状态，所以这类组件就变得很麻烦。
+
+在hooks 之前，官方推荐使用 **渲染属性** 和高阶组件（Higher-Order-Component）我们可以稍微简单看下这两种模式
+
 
--- learn-hooks/src/components/simpleHooks.jsx
+++ learn-hooks/src/components/simpleHooks.jsx
@@ -1,22 +1,40 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-class simpleHooks extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      count: 0
-    }
-  }
-  render() {
-    return (
-      <div>
-        <p>You clicked {this.state.count}</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          click me
-        </button>
-      </div>
-    );
-  }
+// class simpleHooks extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       count: 0
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <p>You clicked {this.state.count}</p>
+//         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+//           click me
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+// export default simpleHooks
+
+// 修改为react-hooks的版本，是不是非常的简单 不用写 constructor 不用写 this.state
+// 直接在函数中解构 一个变量和一个操作变量的 set函数 这里组件变成了一个函数，这个函数确拥有自己的
+// 状态count 同时它还可以更新自己的状态，之所以它这样的厉害，实际上这个函数注入了一个hooks useState
+// hooks 本质上就是一个特殊的函数它可以为我们的函数式组件 注入一些特殊的功能，
+import React, { useState } from 'react'
+function SimpleHooks() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        click me
+      </button>
+    </div>
+  )
 }
 
-export default simpleHooks
+export default SimpleHooks
