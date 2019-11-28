23bbdb4b2bf925d7b2cbeeda924f9b887d3928ad
褚鹏飞 <540788769@qq.com>
2019-11-28 08:59:39 +0800

    添加hooks实现

--- learn-hooks/src/App.js
+++ learn-hooks/src/App.js
@@ -1,79 +1,44 @@
-import React, { useState, PureComponent, useEffect, useRef, useCallback } from 'react'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 import './App.css'
 
-// 函数组件
-// const Count = memo(function Count(props) {
-//   console.log('count render');
-//   return (
-//     <div>{props.count}</div>
-//   )
-// })
 
-class Count extends PureComponent {
-  render() {
-    const { props } = this
-    return (
-      <div>{props.count}</div>
-    )
+function Control() {
+  return <div></div>
+}
+
+function Todos() {
+  return <div></div>
+}
+
+function TodoList() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    // setTodos 传入一个函数避免直接对修改 todos
+    setTodos(todos => [...todos, todo])
   }
-}
 
-function useCount(defaultCount) {
-  const [count, setCount] = useState(defaultCount)
-  let it = useRef()
-  //只执行一次
-  useEffect(() => {
-    it.current = setInterval(() => {
-      setCount(count => count + 1)
-    }, 1000)
-  }, [])
-  // 每次都检查
-  useEffect(() => {
-    if (count >= 10) {
-      clearInterval(it.current)
-    }
-  })
-  return [count]
-}
+  const removeTodo = (id) => {
+    setTodos(todos => todos.filter(todo => {
+      return todo.id !== id
+    }))
+  }
 
-// 自定义hooks的size函数
-
-function useSize() {
-  // 定义数据
-  const [size, setSize] = useState({
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight,
-  })
-  // 定义副作用 只需要执行一次
-  useEffect(() => {
-    window.addEventListener('resize', onResize, false);
-    return () => {
-      window.removeEventListener('resize', onResize, false);
-    }
-  }, [])
-
-  const onResize = useCallback(() => {
-    setSize({
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-    })
-  }, [])
-
-  return size
-}
-
-function App() {
-  const [count] = useCount(0)
-  const size = useSize()
+  const toggleTodo = (id) => {
+    setTodos(todos => todos.map(todo => {
+      return todo.id === id ? {
+        ...todo,
+        complete: !todo.complete,
+      }
+        : todo;
+    }))
+  }
   return (
-    <div>
-      <button>
-        add {count}
-        size {size.width} {size.height}
-      </button>
-      <Count count={count} />
+    <div className="todo-list">
+      <Control addTodo={addTodo} />
+      <Todos removeTodo={removeTodo} toggleTodo={toggleTodo} />
     </div>
   );
 }
 
-export default App;
+export default TodoList;
